19eacb3776302005eb37587b7802853cbf9fae36
Chris Gracia <felemeria@gmail.com>
2024-03-28 00:44:18 -0400

    fix: updated header component. Next: Content and footer. Then links. then tab styling.

--- cheatsheet-app/src/components/Header.js
+++ cheatsheet-app/src/components/Header.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import '../pages/HomePage.css';
+import { Link } from 'react-router-dom'; // If you use Link inside the header
 
-function Header() {
+const Header = () => {
   return (
-    <div className="header">
-        <h1>Cheatsheet Monster</h1>
-        <div className="header-icons">
-          <a href="https://www.buymeacoffee.com/emeria" target="_blank" rel="noopener noreferrer">
-            <img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" style={{ height: '60px', width: '217px' }} />
-          </a><span>&nbsp;</span>
-          <a href="https://www.patreon.com/emeria" target="_blank" rel="noopener noreferrer">
-            <img src={process.env.PUBLIC_URL + "/images/PATREON_SYMBOL_1_BLACK_RGB.png"} alt="Patreon" />
-          </a>
-        </div>
-        <h4>by Thundersphinx</h4>
-    </div>
+    <header>
+      <h1>Cheatsheet Monster</h1>
+      <div className="header-icons">
+        <a href="https://www.buymeacoffee.com/emeria" target="_blank" rel="noopener noreferrer">
+          <img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" style={{ height: '60px', width: '217px' }} />
+        </a><span>&nbsp;</span>
+        <a href="https://www.patreon.com/emeria" target="_blank" rel="noopener noreferrer">
+          <img src={process.env.PUBLIC_URL + "/images/PATREON_SYMBOL_1_BLACK_RGB.png"} alt="Patreon" />
+        </a>
+      </div>
+      <h4>by Thundersphinx</h4>
+    </header>
   );
-}
+};
 
 export default Header;
--- cheatsheet-app/src/pages/HomePage.js
+++ cheatsheet-app/src/pages/HomePage.js
@@ -2,22 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './HomePage.css'; // Make sure to create a CSS file for styling
 import Footer from '../components/Footer';
+import Header from '../components/Header';
 
 const HomePage = () => {
   return (
     <div className="HomePage">
-      <header>
-        <h1>Cheatsheet Monster</h1>
-        <div className="header-icons">
-          <a href="https://www.buymeacoffee.com/emeria" target="_blank" rel="noopener noreferrer">
-            <img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" style={{ height: '60px', width: '217px' }} />
-          </a><span>&nbsp;</span>
-          <a href="https://www.patreon.com/emeria" target="_blank" rel="noopener noreferrer">
-            <img src={process.env.PUBLIC_URL + "/images/PATREON_SYMBOL_1_BLACK_RGB.png"} alt="Patreon" />
-          </a>
-        </div>
-        <h4>by Thundersphinx</h4>
-      </header>
+        <Header />
       <div className="content">
         <h2>Choose a game:</h2>
         <div className="game-links">
