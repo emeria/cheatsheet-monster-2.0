76de73c67f828dfbb293dcce21322021f3177f89
Chris Gracia <felemeria@gmail.com>
2026-09-12 09:13:45 -0400

    Add custom 404 error page with redirect to main application

--- cheatsheet-app/src/index.js
+++ cheatsheet-app/src/index.js
@@ -1,20 +1,17 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client'; // Updated import
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LastEpochPage from './pages/LastEpochPage';
 import PathOfExilePage from './pages/PathofExilePage';
 import HomePage from './pages/HomePage';
-import './index.css'; // Make sure to create a CSS file for styling
+import './index.css';
 
-// Get a reference to the root DOM element
 const rootElement = document.getElementById('root');
-// Create a root
 const root = createRoot(rootElement);
 
-// Render your app
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/cheatsheet-monster-2.0">
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/poe" element={<PathOfExilePage />} />
